e3cec8b4b1371853e430fa2cd11438febb07ebd1
Chika <chikabrown@yahoo.com>
2016-05-22 16:22:47 +0100

    For app

---
+++ node_proj/chatApp/app.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var LocalStrategy = require('passport-local').Strategy;
+
+// used to connect to mongodb from node
+var mongoose = require('mongoose');
+var passport = require('passport');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var mongo = require('mongodb');
+var monk = require('monk');
+
+// nodetest is my database name and add models to be used
+var Account = require('./models/account');
+var db = mongoose.createConnection('localhost:27017/nodetest');
+var app = express();
+
+
+ //passport.use(Account.createStrategy());
+ passport.use(new LocalStrategy(Account.authenticate()));
+ passport.serializeUser(Account.serializeUser());
+ passport.deserializeUser(Account.deserializeUser());
+
+//attach lister to connected event
+mongoose.connection.once('connected', function() {
+    console.log("Connected to database")
+});
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
